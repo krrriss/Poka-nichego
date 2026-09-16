83da811c1c2a1ba8d1af3222f63910e55de9ba84
krrriss <kpuctuna7170@gmail.com>
2026-09-16 19:10:42 +0500

    Написал программу Helloworld

---
+++ privet/privet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace privet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, world!");
+            Console.ResetColor();
+
+
+            //Console.ReadKey();
+        }
+    }
+}
