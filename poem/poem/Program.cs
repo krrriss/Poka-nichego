dab9bed2b45616a6dd7c1b39e8be955573eb0e9d
krrriss <kriss14020812@gmail.com>
2026-09-22 13:55:13 +0500

    Написала стихотворение

---
+++ poem/poem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Корней Чуковский");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Закаляка");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Дали Мурочке тетрадь,\r\nСтала Мура рисовать.\r\n«Это — козочка рогатая.\r\nЭто — ёлочка мохнатая.\r\nЭто — дядя с бородой.\r\nЭто — дом с трубой».");
+
+            Console.WriteLine("«Ну, а это что такое,\r\nНепонятное, чудное,\r\nС десятью ногами,\r\nС десятью рогами?»");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("«Это Бяка-Закаляка\r\nКусачая,\r\nЯ сама из головы её выдумала».");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("«Что ж ты бросила тетрадь,\r\nПерестала рисовать?»");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("«Я её боюсь!»");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ poem/poem/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("poem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("poem")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("937956a2-9683-4b21-b44c-2e9b8499858a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
